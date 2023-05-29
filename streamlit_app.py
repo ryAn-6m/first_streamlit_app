1f84c7412654bca792678b32edfaec04f4c79485
ryAn-6m <91593939+ryAn-6m@users.noreply.github.com>
2023-05-29 14:44:25 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-                
+streamlit.header('My Parents New Healthy Diner')          
+streamlit.text('Omega 3 3 & Blueberry Otmeal')
+streamlit.text('Kale, Spinach & ROcket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')                
     

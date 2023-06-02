68ee03d191e1a2df50eccbfd548b74a47d6f05e8
ryAn-6m <91593939+ryAn-6m@users.noreply.github.com>
2023-06-02 19:22:38 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,5 +49,5 @@ my_data_row = my_cur.fetchone()
 streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_row)
 
-select * from pc_rivery_db.public.fruit_load_list
+# select * from pc_rivery_db.public.fruit_load_list
 

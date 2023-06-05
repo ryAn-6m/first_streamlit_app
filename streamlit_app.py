ba8a994bf94904444c87d5d32f59e93b801f401d
ryAn-6m <91593939+ryAn-6m@users.noreply.github.com>
2023-06-05 19:25:01 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,9 +74,9 @@ import requests
 # streamlit.dataframe(my_data_rows)
 
 # Allow the end user to add a fruit to the list
-# fruit_choice = streamlit.text_input('What fruit would you like to add?','jackfruit')
-# my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-# streamlit.write('Thanks for adding ', fruit_choice)
+fruit_choice = streamlit.text_input('What fruit would you like to add?','jackfruit')
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+streamlit.write('Thanks for adding ', fruit_choice)
 
 # streamlit.write('Thanks for adding ', add_my_fruit)
 # my_cur.execute("insert into fruit_load_list values ('from streamlit')")
@@ -90,10 +90,10 @@ def get_fruit_load_list():
   return my_cur.fetchall()
  
 #  #Add a button to load the frui
-# if streamlit.button('Get Fruit Load List'):
-#   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-#   my_data_rows = get_fruit_load_list()
-#   streamlit.dataframe(my_data_rows)
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
   
  #   Allow the end user to add a fruit to the list
 def insert_row_snowflake(new_fruit):

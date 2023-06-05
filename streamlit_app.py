000d899f56c12dc3f3a21d1a8cb11e749919e85f
ryAn-6m <91593939+ryAn-6m@users.noreply.github.com>
2023-06-05 17:26:34 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,7 +66,7 @@ try:
 # streamlit.text("Hello from Snowflake:")
 # streamlit.text(my_data_row)
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+# my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()

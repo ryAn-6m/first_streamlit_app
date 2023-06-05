982c72291ffd307f1db4b4280b4b47170b3fcdc5
ryAn-6m <91593939+ryAn-6m@users.noreply.github.com>
2023-06-05 12:23:06 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,8 +16,8 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-# streamlit.multiselect("Pick some fruits:")
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+streamlit.multiselect("Pick some fruits:")
+# my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
@@ -28,8 +28,8 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?')
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-# streamlit.write('The user entered ', fruit_choice)
+# my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+streamlit.write('The user entered ', fruit_choice)
 
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())

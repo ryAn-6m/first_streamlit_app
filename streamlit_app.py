ba3a1e33958bedeeee21a68774263ad30caf46c2
ryAn-6m <91593939+ryAn-6m@users.noreply.github.com>
2023-06-06 18:31:46 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -81,7 +81,7 @@ streamlit.header("The fruit load list contains:")
 def get_fruit_load_list(add_my_fruit):
  with my_cnx.cursor() as my_cur:
   my_cur.execute("select * from fruit_load_list")
-  return my_cur.clear()
+  return my_cur.fetchall().clear()
  
 #  #Add a button to load the frui
 # if streamlit.button('Get Fruit Load List'):
